40dda567ce223e29e9e2395484ddf5068496bd89
jssatorres <juansataz.dev@gmail.com>
2023-06-21 20:44:32 +0200

    fix: bugfooter copie

--- src/components/footer copy/Footer.jsx
+++ src/components/footer copy/Footer.jsx
@@ -1,21 +1,20 @@
+import React from 'react'
 import SocialMediaIcons from '../socialMediaIcons'
 
-const Footer = () => {
-  return (
-    <footer className='h-64 bg-red pt-10'>
-      <div className='w-10/12 mx-auto'>
-        <SocialMediaIcons />
-        <div className='md:flex justify-center md:justify-between text-center '>
-          <p className='font-playfair font-semibold text-2xl text-yellow'>
-            JANE ESPER
-          </p>
-          <p className='font-playfair text-md text-yellow'>
-            ©2022 ESPER. All Rights Reserved.
-          </p>
-        </div>
+const Footer = () => (
+  <footer className='h-64 bg-red pt-10'>
+    <div className='w-10/12 mx-auto'>
+      <SocialMediaIcons />
+      <div className='md:flex justify-center md:justify-between text-center '>
+        <p className='font-playfair font-semibold text-2xl text-yellow'>
+          JANE ESPER
+        </p>
+        <p className='font-playfair text-md text-yellow'>
+          ©2022 ESPER. All Rights Reserved.
+        </p>
       </div>
-    </footer>
-  )
-}
+    </div>
+  </footer>
+)
 
 export default Footer
